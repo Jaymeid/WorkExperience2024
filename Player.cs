cd4599f278d021e4d0f8a24870cea0a3f9de5a62
james <jamespawson64@gmail.com>
2024-07-15 22:22:33 +0100

    Implement inventory system Add health potion item to game Add ability to pick up and use items Add items to room

--- Player.cs
+++ Player.cs
@@ -1,10 +1,16 @@
 using System.Security.Cryptography;
+using System.Collections.Generic;
 
 class Player : Character{
 
+    private List<Item> inventory;
+    private int inventorySize;
     public Player(){
         health = 100;
         attackDamage = 10;
+
+        inventorySize = 5;
+        inventory = new List<Item>();
     }
 
     public bool BlockAttack(){
@@ -21,6 +27,20 @@ class Player : Character{
     public override void Die(){
         Console.WriteLine("You have succumbed to the monsters.");
         Environment.Exit(0);
+    }
+
+    public void AddHealth(int healthToAdd){
+        health += healthToAdd;
+    }
+
+    public void PickUpItem(Item newItem){
+        if(inventory.Count < 5){
+            Console.WriteLine($"{newItem.name} added to inventory");
+            inventory.Add(newItem);
+        }
+        else{
+            Console.WriteLine($"Can't pick up the {newItem.name}. Not enough inventory space!");
+        }
     }
 
 }
