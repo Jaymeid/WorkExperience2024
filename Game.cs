196db3f1cc20c1f15ff1bbaeaafcb1f2c3deb911
Lewis&Reshma <jamespawson65@gmail.com>
2024-07-16 15:44:30 +0100

    add shop

--- Game.cs
+++ Game.cs
@@ -16,27 +16,37 @@ class Game
 
     private void InitializeRooms()
     {
-        rooms = new Dictionary<string, Room>();
+        rooms = [];
 
-        Room livingRoom = new Room("Living Room", "You are in a cozy living room. There is a sofa and a TV here.", false);
-        Room kitchen = new Room("Kitchen", "You are in a kitchen. There is a fridge and a stove here.", true);
-        Room bedroom = new Room("Bedroom", "You are in a bedroom. There is a bed and a wardrobe here.", false);
+        Room livingRoom = new("Living Room", "You are in a cozy living room. There is a sofa and a TV here.", false);
+        Room kitchen = new("Kitchen", "You are in a kitchen. There is a fridge and a stove here.", true);
+        Room bedroom = new("Bedroom", "You are in a bedroom. There is a bed and a wardrobe here.", false);
+        Room shop = new("Shop", "You have entered the shop. You can buy new items here to help you along the way!", false);
 
         livingRoom.AddAdjacentRoom("north", kitchen);
         kitchen.AddAdjacentRoom("south", livingRoom);
         kitchen.AddAdjacentRoom("east", bedroom);
         bedroom.AddAdjacentRoom("west", kitchen);
+        shop.AddAdjacentRoom("north", bedroom);
+        shop.AddAdjacentRoom("west", livingRoom);
+        livingRoom.AddAdjacentRoom("east", shop);
+        bedroom.AddAdjacentRoom("south", shop);
 
         rooms.Add("Living Room", livingRoom);
         rooms.Add("Kitchen", kitchen);
         rooms.Add("Bedroom", bedroom);
+        rooms.Add("Shop", shop);
 
+        foreach (KeyValuePair<string, Room> kvp in rooms)
+        {
+            Console.WriteLine(kvp);
+        }
         currentRoom = livingRoom;
     }
 
     public void Start()
     {
-        
+
 
         Console.WriteLine("Welcome to the Text-Based Room Explorer!");
         Console.WriteLine("Type 'help' to see the list of commands.");
@@ -46,7 +56,9 @@ class Game
         while (true)
         {
             Console.Write("> ");
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
             string input = Console.ReadLine().ToLower();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
             string[] inputParts = input.Split(' ');
 
             switch (inputParts[0])
@@ -55,10 +67,12 @@ class Game
                     if (inputParts.Length > 1)
                     {
                         MoveToRoom(inputParts[1]);
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
                         if (currentRoom.GetHasMonster())
                         {
                             CombatLoop();
                         }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
                     }
                     else
                     {
@@ -85,7 +99,7 @@ class Game
     {
         bool continueBattle = true;
 
-        Troll troll = new Troll();
+        Troll troll = new();
 
         Console.WriteLine($"Oh no! You have encountered a {troll.GetCharacterName()}!\nYou must now battle to the death!");
 
@@ -93,7 +107,12 @@ class Game
         {
             Console.WriteLine("What is your move?");
 
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
             string input = Console.ReadLine().ToLower();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+
             string[] inputParts = input.Split(' ');
 
             bool playerHasBlocked = false;
@@ -153,11 +172,14 @@ class Game
         }
 
         Console.WriteLine($"The {troll.GetCharacterName()} has been vanquished!\n");
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
         currentRoom.SetHasMonster(false);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
     }
 
     private void MoveToRoom(string direction)
     {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
         if (currentRoom.AdjacentRooms.ContainsKey(direction))
         {
             currentRoom = currentRoom.AdjacentRooms[direction];
@@ -167,11 +189,14 @@ class Game
         {
             Console.WriteLine("You can't go that way.");
         }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
     }
 
     private void DisplayCurrentRoom()
     {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
         Console.WriteLine(currentRoom.Description);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
     }
 
     private void DisplayHelp()
@@ -181,7 +206,8 @@ class Game
         Console.WriteLine("  look - Look around the current room");
         Console.WriteLine("  help - Display this help message");
         Console.WriteLine("  attack - Takes 10HP away from enemy");
-        Console.WriteLine("  block - gives an 3/8 chance to block the enemy's attacks");
+        Console.WriteLine("  block - Gives an 3/8 chance to block the enemy's attacks");
+        Console.WriteLine("  shop - Hint (go east from the living room or south from the bedroom to enter the shop)");
         Console.WriteLine("  quit - Exit the game");
     }
 }
